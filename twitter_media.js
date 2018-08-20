eb076a733e6e8c338aa742261112a25ede7af6f9
Yasu <yasu@pixiv.co.jp>
2018-08-21 02:51:43 +0900

    change scraping method; scraping from user media page

--- twitter_media.js
+++ twitter_media.js
@@ -16,24 +16,27 @@ const scroll = async (page, length) => {
   let owari = false
   let num = 0
   let images
+  //const selector = 'span.AdaptiveStreamGridImage'
+  const selector = '.stream-item[data-item-type=\"tweet\"]'
   while (true) {
     if (getRandomInt(100) <= 5) {
-      await page.evaluate(() => window.scrollBy(0,-10))
+      await page.evaluate(() => window.scrollBy(0, -10))
     } else {
-      await page.evaluate(() => window.scrollBy(0,10))
+      // await page.evaluate(() => window.scrollBy(0, 10))
+      await page.evaluate(() => window.scrollBy(0, 10000), {'waitUntil': 'networkidle0'})
     }
 
-    images = await page.evaluate(() => {
-      const list = Array.from(document.querySelectorAll('span.AdaptiveStreamGridImage'))
+    images = await page.evaluate(selector => {
+      const list = Array.from(document.querySelectorAll(selector))
       return list.map(e => e.dataset.itemId)
-    })
+    }, selector)
     count += 1
 
     if (images.length > length) {
       break
     }
 
-    if (count >= 10000) {
+    if (count >= 10) {
       owari = true
       break
     }
@@ -42,21 +45,16 @@ const scroll = async (page, length) => {
   return {'owari': owari, 'images': images}
 }
 
-const extractMediaUrls = async (name) => {
-  const browser = await puppeteer.launch({
-      headless: true
-  });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1366, height: 768})
-  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.32 Safari/537.36')
-
+const extractMediaUrls = async (page, name) => {
   await page.goto('https://twitter.com')
 
-  const url = `https://twitter.com/search?f=images&vertical=default&q=from%3A%40${name}&src=typd`
+  // const url = `https://twitter.com/search?f=images&vertical=default&q=from%3A%40${name}&src=typd`
+  const url = `https://twitter.com/${name}/media`
   await page.goto(url);
   await page.waitForNavigation({waitUntil: 'networkidle0'})
 
-  selector = 'span.AdaptiveStreamGridImage'
+  // selector = 'span.AdaptiveStreamGridImage'
+  selector = '.stream-item[data-item-type=\"tweet\"]'
   await page.waitFor(selector)
   const length = await page.evaluate(selector => document.querySelectorAll(selector).length, selector)
 
@@ -66,19 +64,38 @@ const extractMediaUrls = async (name) => {
     owari = ret.owari
     images = ret.images
 
-    if (images.length > 40) {break}
+    if (images.length > 100) {break}
   }
-  await browser.close();
-
   return new Promise(resolve => resolve(images))
 }
 
 
 (async () => {
-  const users = fs.readFile('users.txt', 'utf-8', async (err, name) => {
-    if (err) throw err
+  const names = fs.readFileSync('users.txt', 'utf-8').toString().split("\n")
 
-    const list = await extractMediaUrls(name)
-    console.log(list)
+  const browser = await puppeteer.launch({
+      headless: false
   })
-})();
+
+  let page = await browser.newPage()
+  await page.setViewport({ width: 1366, height: 768})
+  await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.32 Safari/537.36')
+
+  for (let j = 0; j < names.length; j++) {
+    const name = names[j]
+    const status_ids = await extractMediaUrls(page, name)
+    let image_urls = []
+    for (let i = 0; i < status_ids.length; i++) {
+        await page.goto(`https://twitter.com/${name}/status/${status_ids[i]}`, {'waitUntil': 'networkidle0'})
+        await page.waitForNavigation({waitUntil: 'networkidle0'})
+        const urls = await page.evaluate(() => {
+          const urls = Array.from(document.querySelectorAll('[data-image-url]'), e => e.dataset.imageUrl)
+          return urls
+        })
+        console.log(urls)
+        image_urls.push(new Promise(resolve => resolve(urls)))
+    }
+    console.log(image_urls)
+  }
+  await browser.close()
+})()
